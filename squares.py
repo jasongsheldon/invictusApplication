aa2475b7c03641bcb090dc09bb99b9bbd31349e9
jasonsheldon <jason.sheldon@trintel.co.za>
2020-04-03 19:22:44 +0200

    initial working: no nameko used yet but installed and pytest and some tests

---
+++ squares.py
@@ -0,0 +1,34 @@
+
+def test_is_integer(test_item):
+    """
+        Function to test if an inputted item is an integer or not
+        Args: test_item (any type from list)
+        Output: (boolean) true if an integer or false if not 
+    """
+    try:
+        int(test_item)
+        return True
+    except:
+        return False
+
+def square_of_odds(integers_input):
+    # create a new list of only integers in the provided list
+    # and check if the length of the inout list matches this
+    # if it does not match it means there were non integers in the list
+    # and thus invalid input
+    checked_list = [x for x in integers_input if test_is_integer(x)]
+    if len(checked_list) != len(integers_input):
+        return {"status": False, "error": "List does not contain only integers"}
+    else:
+        odds_list = [x for x in integers_input if x % 2 != 0]
+        if len(odds_list) < 1:
+            return {"status": False, "error": "List does not contain any odd numbers"}
+        else:
+            total = 0
+            for item in odds_list:
+                total = total + item**2
+            return {"status": True, "data": total}
+
+if __name__ == "__main__":
+    print(square_of_odds([1, 3, 12, 13, 14, 19, 20]))
+    
---
+++ test_squares.py
@@ -0,0 +1,17 @@
+from squares import square_of_odds
+
+
+def test_invalid_data():
+    """Test to see if there is invalid (non integer) data in the list
+    """
+    assert square_of_odds([1,2,3,'a']).get('status') == False
+
+def test_none_odd():
+    """Test to see if there are any odd numbers in the list as we are summing odd numbers
+       so if are none its bad data
+    """
+    assert square_of_odds([2,4,6,8]).get('status') == False
+
+def test_as_expected():
+    """Positive test to see that the correct sum of squares for valid data is returned"""
+    assert square_of_odds([1, 3, 12, 13, 14, 19, 20]).get('data') == 540 
